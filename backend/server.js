4bfefe86590bdeaab4067f8544fb96376b644c9b
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-09 05:03:37 +0530

    Setup mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDb.js";
 
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Method to test by logging any route that is hit
 app.use((req, res, next) => {
@@ -15,4 +18,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
+    connectMongoDB();
 });
