a491fcd7fa1432fb8ce680c157490256e17e6a4d
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-09 04:34:29 +0530

    Setup express boilerplate

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Server is running...");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Method to test by logging any route that is hit
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next(); // Ensure next() is called
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
