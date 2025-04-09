e180a3791de8e7e379fc5d769c02f2e6ec2e3c52
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-09 07:18:44 +0530

    Setup shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function App() {
-    return <div className="font-extrabold">Home - Blink</div>;
+    return (
+        <div className="font-extrabold">
+            Home - Blink
+            <div className="h-8 w-28 bg-amber-100"></div>
+            <Button variant="outline" size={"lg"}>
+                Click
+            </Button>
+            <Avatar>
+                <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+        </div>
+    );
 }
 
 export default App;
