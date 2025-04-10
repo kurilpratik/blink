82fa9b4cf444ce23e7bf29597f085dee70d70952
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-10 20:33:43 +0530

    Add logout

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,7 +1,34 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
-    return <div>Dashboard</div>;
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        const response = await fetch("http://localhost:5000/api/auth/logout", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            credentials: "include",
+        });
+
+        const data = await response.json();
+        if (!response.ok) {
+            console.error(data.error);
+            return;
+        } else {
+            console.log("Logout successful:", data);
+            navigate("/");
+        }
+    };
+    return (
+        <div>
+            <Button variant={"outline"} onClick={handleLogout}>
+                Logout
+            </Button>
+        </div>
+    );
 }
 
 export default Dashboard;
